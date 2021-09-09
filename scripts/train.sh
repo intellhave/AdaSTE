61f3ae015d54734101702740de65146841f11490
intellhave <intellhave@gmail.com>
2021-09-09 16:39:57 +0200

    annealing of delta

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,6 @@
 # Train a Full-Precision ResNet
 DEPTH=20
-python my_main.py --model resnet --model_config "{'depth': $DEPTH}" --save resnet_FenBP"$DEPTH" --dataset cifar10 --batch-size 128 --gpu 0 --epochs 300 --tb_dir tb/CIFAR_ResNet"$DEPTH"_FenBP
+python my_main.py --model resnet --model_config "{'depth': $DEPTH}" --save resnet_FenBP"$DEPTH" --dataset cifar10 --batch-size 128 --gpu 0 --epochs 300 --tb_dir tb/CIFAR_ResNet"$DEPTH"_FenBP 
 
 
 # DEPTH=20
