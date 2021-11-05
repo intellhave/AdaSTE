a2ad7bc8785e31360353da86aa2726a696bbf6ad
intellhave <intellhave@gmail.com>
2021-11-05 14:22:49 +0100

    .

--- BayesNN/scripts/RESNET18_cifar10.sh
+++ BayesNN/scripts/RESNET18_cifar10.sh
@@ -1,6 +1,7 @@
 for i in 100
 do
     python main_cifar10.py --model RESNET18 --optim FenBP --experiment-id $i --seed $i
+    #python main_cifar10.py --model RESNET18 --optim BayesBiNN --experiment-id $i --seed $i
 done
 # for i in 100 200 300 400 500
 # do
--- BayesNN/scripts/VGG16_cifar100.sh
+++ BayesNN/scripts/VGG16_cifar100.sh
@@ -1,4 +1,4 @@
-for i in 100 200 300 400 500
+for i in 100
 do
-    python main_cifar100.py --model VGG16 --optim BayesBiNN --experiment-id $i --seed $i
+    python main_cifar100.py --model VGG16 --optim FenBP --experiment-id $i --seed $i
 done
