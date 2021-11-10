f1e454b9770c64949f7fcb89f7b69d9c79875d67
intellhave <intellhave@gmail.com>
2021-11-10 18:36:32 +0100

    added script for tiny img

--- BayesNN/scripts/RESNET18_tinyimg.sh
+++ BayesNN/scripts/RESNET18_tinyimg.sh
@@ -1,6 +1,6 @@
 for i in 100
 do
-    python main_tinyimg.py --model RESNET18 --optim FenBP --experiment-id $i --seed $i
+    python main_tinyimg.py --model RESNET18 --optim BayesBiNN --experiment-id $i --seed $i --batch-size 128 --lr 3e-4 --beta_inc_rate 1.009
     #python main_cifar10.py --model RESNET18 --optim BayesBiNN --experiment-id $i --seed $i
 done
 # for i in 100 200 300 400 500
